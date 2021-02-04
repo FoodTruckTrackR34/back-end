8ec3eb335b0217399a0e1999a90bee789c8eb3d7
Joshua Lovins <joshualovins@Joshuas-MBP.lan>
2021-02-03 21:50:23 -0500

    truckName column migrated in, and now all truck find functions return truckName. Add and put should be unchanged, as far as I can tell.

--- database/migrations/20210203214332_fifth.js
+++ database/migrations/20210203214332_fifth.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.alterTable("trucks", tbl => {
+        tbl.string("truckName", 128)
+      })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.alterTable("trucks", tbl => {
+        tbl.dropColumn("truckName")
+      })
 };
